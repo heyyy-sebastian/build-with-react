c03c2c3bf37de5c40000d9043a2b00e55d426a75
Hailey Miller <hailey.miller91@gmail.com>
2017-05-27 14:01:40 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+var ipsumText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.';
+
+ReactDOM.render(
+  <div>
+    // Your work here
+  </div>,
+  document.getElementById('impl')
+);
