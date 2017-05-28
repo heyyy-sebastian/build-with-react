32430c9fbd3ce3cc129fabe7710d8e2156cf7821
Hailey Miller <hailey.miller91@gmail.com>
2017-05-28 15:23:52 -0400

    git push origin master

--- src/index.js
+++ src/index.js
@@ -2,14 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//Note: this is just a file where I'm saving my answers, no all of these components
+//will render correctly (or at all) if compiled together without commenting out
+//the components for each excersize
+
+//Exercise 1 - JSX
 var ipsumText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.';
 
 ReactDOM.render(
   <div>
     <div>
     <div href="#" className="button">Button</div>
-    <p>{ipsumText}</p>
+    <div>{ipsumText}</div>
   </div>,
   </div>,
-  document.getElementById('impl')
+  document.getElementById('impl'),
+  <vacancySign />,
+   document.body
 );
+
+// Exercise 2 - Props: Create a component for a vacancy sign
+var VacancySign = React.createClass({
+  render: function() {
+    return <div>{this.props.hasvacancy ? '' : 'No '}Vacancy</div>;
+  }
+});
+
+ReactDOM.render(
+  <VacancySign hasvacancy={false} />,
+  document.getElementById('container')
+);
+
