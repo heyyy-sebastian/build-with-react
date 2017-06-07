3b0cc83dabe09000a82d28fc0e87452917e00007
mermatriarchy <hailey.miller91@gmail.com>
2017-06-07 17:19:31 -0400

    add click event exercise

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 //Note: this is just a file where I'm saving my answers, no all of these components
 //will render correctly (or at all) if compiled together without commenting out
-//the components for each excersize
+//the other components for each exersize
 
 //Exercise 1 - JSX
 var ipsumText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.';
@@ -33,3 +33,34 @@ ReactDOM.render(
   document.getElementById('container')
 );
 
+// Exercise 3 - Events: Make the button do stuff on click!
+
+var ChildComponent = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div className="prompt">Add a click handler to this button so that when clicked, performMagic is called in the parent component.</div>
+        <button onClick={this.props.onDoMagic}>Do Magic</button>
+      </div>
+    );
+  }
+});
+
+var ParentComponent = React.createClass({
+  performMagic: function() {
+    alert('TAADAH!');
+  },
+
+  render: function() {
+    return (
+      <div>
+        <ChildComponent onDoMagic={this.performMagic} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <ParentComponent />,
+  document.getElementById('container')
+);
