497ea76781bb94975700e02292a54fd406973cbc
Hailey Miller <hailey.miller91@gmail.com>
2017-06-08 23:38:32 -0400

    add exercise 4 partial work - set initial state

--- src/index.js
+++ src/index.js
@@ -64,3 +64,53 @@ ReactDOM.render(
   <ParentComponent />,
   document.getElementById('container')
 );
+
+// Exercise 4: State - Set the state so the clicked tile is highlighted
+var Board = React.createClass({
+  render: function() {
+    var className = "board";
+    if (this.props.selected) {
+      className += " selected";
+    }
+    return (
+      <div className={className}>
+        {this.props.index + 1}
+      </div>
+    );
+  }
+});
+
+var BoardSwitcher = React.createClass({
+  getInitialState: function() {
+    // first board is selected
+    return {
+      selectedIndex: 0
+    }
+  },
+
+  onToggleClick: function(evt) {
+    //do stuff
+  },
+
+  render: function() {
+    var boards = [];
+    for (var ii = 0; ii < this.props.numBoards; ii++) {
+      var isSelected = ii === 0;
+      boards.push(
+        <Board index={ii} selected={isSelected} key={ii} />
+      );
+    }
+
+    return (
+      <div>
+        <div className="boards">{boards}</div>
+        <button>Toggle</button>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <BoardSwitcher numBoards={3} />,
+  document.getElementById('container')
+);
